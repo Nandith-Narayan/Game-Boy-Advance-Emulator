40faaa4297289166c6d2694f3c4f5a3ae95beb79
nandi <nandith2ndemail@gmail.com>
2026-04-12 12:54:04 -0400

    m3_demo.gba debug

--- src/main.rs
+++ src/main.rs
@@ -38,25 +38,27 @@ fn main() {
         });
     window.set_target_fps(60);
     let mut mem = memory::init();
-    mem.load_rom(String::from("C:/GBA roms/first.gba"));
+    mem.load_rom(String::from("C:/GBA roms/m3_demo.gba"));
     let mut cpu = cpu::init();
     let mut ppu = ppu::init();
     //let now = Instant::now();
-    let n = 2000000;
+    let n = 1_000_000;//447795;//447795;
     /*for address in 0..4{
         print!("{:#4x}", cpu.memory.read_8(address+0x8000000))
     }
     println!("\n {:#x}",cpu.memory.read_32(0x8000000));*/
-
-    for _ in 1..n {
+    for _i in 1..n {
         //println!("R[7] = {:#4x} ({})", cpu.r[7], cpu.r[7]);
         //println!("R[12] = {:#4x} ({})", cpu.r[12], cpu.r[12]);
-        //println!("R[0] = {:#4x}, R[1] = {:#4x}, R[2] = {:#4x}, R[3] = {:#4x}, R[4] = {:#4x}, R[5] = {:#4x}, SP = {:#4x}, LR = {:#4x}, PC = {:#4x}, Carry Flag: {}", cpu.r[0], cpu.r[1], cpu.r[2], cpu.r[3], cpu.r[4], cpu.r[5],cpu.r[13], cpu.r[14], cpu.r[15], cpu.c);
+        /*if cpu.r[15]==0x080002FC{
 
+            println!("R[0] = {:#4x}, R[1] = {:#4x}, R[2] = {:#4x}, R[3] = {:#4x}, R[4] = {:#4x}, R[5] = {:#4x}, SP = {:#4x}, LR = {:#4x}, PC = {:#4x}, Carry Flag: {}", cpu.r[0], cpu.r[1], cpu.r[2], cpu.r[3], cpu.r[4], cpu.r[5], cpu.r[13], cpu.r[14], cpu.r[15], cpu.c);
+        }*/
         /*if cpu.r[7]>200{
             break;
         }*/
         //println!("R[0] = {:#4x} ({})", cpu.r[0], cpu.r[0]);
+
         cpu.tick_cycle(&mut mem);
         ppu.tick_cycle(&mut mem);
     }
@@ -71,6 +73,7 @@ fn main() {
     /*let val = now.elapsed().as_nanos();
     println!("Took {} ns to execute {} instructions. ({} ns per instruction)", val, n, val/n);*/
     println!("R[12] = {:#4x} ({})", cpu.r[12], cpu.r[12]);
+    println!("R[7] = {:#4x} ({})", cpu.r[7], cpu.r[7]);
     for address in 0..=0x30{
         print!("{:#4x}", mem.wram_on_board[address])
     }
