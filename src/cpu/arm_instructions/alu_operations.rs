1130ca8af85bcb06711abda3df5944132368368c
nandi <nandith2ndemail@gmail.com>
2026-04-17 17:14:46 -0400

    Fixed set flag logic for BIC op

--- src/cpu/arm_instructions/alu_operations.rs
+++ src/cpu/arm_instructions/alu_operations.rs
@@ -242,9 +242,10 @@ impl Cpu{
             14 => {
                 // BIC (Bit Clear)
                 let result = operand1 & (!operand2);
-
-                self.z = result == 0;
-                self.n = (result & 0x80000000) != 0;
+                if set_flags {
+                    self.z = result == 0;
+                    self.n = (result & 0x80000000) != 0;
+                }
                 self.set_r(rd as usize, result);
             },
             15 => {
